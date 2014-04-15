66709f8e6491c4537763af3af28b20fae85b2de7
Fábio Santos <fabiosantosart@gmail.com>
2014-04-15 01:32:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# wentto [![Build Status](https://secure.travis-ci.org/fabiosantoscode/wentto.png?branch=master)](http://travis-ci.org/fabiosantoscode/wentto)
+
+GOTO in Javascripts. Considered harmful.
+
+## Getting Started
+Install the module with: `npm install wentto`
+
+```javascript
+var wentto = require('wentto');
+wentto.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Fábio Santos. Licensed under the MIT license.
---
+++ test/wentto_test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var wentTo = require('../lib/wentto.js');
+
+/*
+    ======== A Handy Little Nodeunit Reference ========
+    https://github.com/caolan/nodeunit
+
+    Test methods:
+        test.expect(numAssertions)
+        test.done()
+    Test assertions:
+        test.ok(value, [message])
+        test.equal(actual, expected, [message])
+        test.notEqual(actual, expected, [message])
+        test.deepEqual(actual, expected, [message])
+        test.notDeepEqual(actual, expected, [message])
+        test.strictEqual(actual, expected, [message])
+        test.notStrictEqual(actual, expected, [message])
+        test.throws(block, [error], [message])
+        test.doesNotThrow(block, [error], [message])
+        test.ifError(value)
+*/
+
+exports.wentto = {
+    setUp: function(done) {
+        // setup here
+        done();
+    },
+    'return': function (t) {
+        t.expect(1)
+        t.equal(typeof wentTo([function(){}]), 'function', 'should return a function')
+        t.done()
+    },
+    'function still works': function (t) {
+        var func = wentTo([function testFunc_withBlocks(go, a) {
+            switch(a) {
+                case 2:
+                    return 2
+            }
+            if(a === 1) { return 1 }
+        }])
+
+        t.equal(func(1), 1)
+        t.equal(func(2), 2)
+        t.done()
+    },
+    'executes functions in series': function (t) {
+        var a = 0
+
+        wentTo([
+            function () { a+= 1 },
+            function () { a+= 1 },
+            function () { a+= 1 }
+        ])()
+
+        t.equal(a, 3)
+        t.done()
+    },
+    'simple going-to': function(t) {
+        var arr = []
+        wentTo([
+            function (go) { arr.push(1); return go(2); },
+            function () { arr.push(3); return null; },
+            function (go) { arr.push(2); return go(1); }
+        ])();
+
+        t.deepEqual(arr, [1, 2, 3])
+        t.done()
+    }
+};
